0cdca12bafbde2092dc95cd10021b89c57d21e93
mahmoud <ramadanmahmoud945@gmail.com>
2024-04-14 14:04:07 +0200

    2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 1:
+        print("{} arguments.".format(len(argv) - 1))
+    elif len(argv) == 2:
+        print("{} argument:".format(len(argv) - 1))
+        print("{} : {}".format((len(argv) - 1), argv[1]))
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+        i = 1
+        while i < len(argv):
+            print("{}: {}".format(i, argv[i]))
+            i += 1
