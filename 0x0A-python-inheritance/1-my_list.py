0199eaf824b10db9d68e3febc5f26dd0aaa6c07f
mahmoud <ramadanmahmoud945@gmail.com>
2024-04-16 18:28:35 +0200

    1-

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+""" class inheritance """
+
+
 class MyList(list):
+    """ initiation """
     def __init__(self):
         return super().__init__()
+
+    """ print sorted """
 
     def print_sorted(self):
         print(sorted(self))
