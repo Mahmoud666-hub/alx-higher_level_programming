9c49510eaa2a1779bf055e4a1858251e9fac83ac
mahmoud <ramadanmahmoud945@gmail.com>
2024-04-17 19:57:43 +0200

    6-6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,10 +5,10 @@
 def print_matrix_integer(matrix=[[]]):
     """ handle empty parameter """
     if len(matrix) == 1:
-        print("{}".format("\n"))
+        print()
     else:
         for i in matrix:
             for m in i:
                 if i.index(m) != len(i) - 1:
-                    print("{}".format(m), end=" ")
-            print("{}".format(m))
+                    print("{:d}".format(m), end=" ")
+            print("{:d}".format(m))
