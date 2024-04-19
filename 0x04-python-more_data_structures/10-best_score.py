62b9a047b3ef40651a5f77c93682b55f3ad6337e
mahmoud <ramadanmahmoud945@gmail.com>
2024-04-19 19:40:45 +0200

    10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == {}:
+    if a_dictionary == {} or a_dictionary is None:
         return None
     x = 0
     for i, m in a_dictionary.items():
