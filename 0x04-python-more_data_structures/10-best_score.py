7f3774c73208932127dcedd77a27040e12241e53
mahmoud <ramadanmahmoud945@gmail.com>
2024-04-19 19:36:49 +0200

    10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    x = 0
+    for i, m in a_dictionary.items():
+        if x < m:
+            x = m
+            y = i
+    return y
