af6a41e9b3acd2bf143174286253bc23ca5b5454
mahmoud <ramadanmahmoud945@gmail.com>
2024-04-21 12:45:01 +0200

    12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) != str:
+        return 0
+    h = {"M": 1000, "D": 500, "C": 100, "L": 50,
+         "X": 10, "V": 5, "I": 1}
+    n = 0
+    z = 0
+    while z < len(roman_string):
+        if roman_string[z:(z+2)] == "IX":
+            n += 9
+            z += 2
+        elif roman_string[z:(z+2)] == "IV":
+            n += 4
+            z += 2
+        elif roman_string[z:(z + 1)] in h:
+            n += h[roman_string[z:(z + 1)]]
+            z += 1
+    return n
