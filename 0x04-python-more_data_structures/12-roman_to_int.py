1b38432952e8d2ae5dbfa8987d7e3a2c50d93da5
mahmoud <ramadanmahmoud945@gmail.com>
2024-04-22 12:00:50 +0200

    n

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ def roman_to_int(roman_string):
     h = {"M": 1000, "D": 500, "C": 100, "L": 50,
          "X": 10, "V": 5, "I": 1}
     q = {"IX": 9, "IV": 4, "XC": 90, "CM": 900,
-         "CD": 400, "LX": 40}
+         "CD": 400, "XL": 40}
     n = 0
     z = 0
     while z < len(roman_string):
