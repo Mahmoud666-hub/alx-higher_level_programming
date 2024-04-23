4c8076e250947f3303e3cc21fab76c7dc39012ed
mahmoud <ramadanmahmoud945@gmail.com>
2024-04-23 22:32:34 +0200

    2

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
             for f in my_list[:x]:
                 print("{:d}".format(f), end="")
             print()
-            # print("1")
+            print("1")
             return x
     except IndexError:
         m = 0
@@ -13,5 +13,6 @@ def safe_print_list(my_list=[], x=0):
             print("{:d}".format(f), end="")
             m += 1
         print()
+        # print(f"\n{my_list.index(my_list[-1]) + 1}")
         # print("2")
         return m
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     try:
-        if range(my_list[:x]):
             m = 0
-            for f in my_list[:x]:
-                if isinstance(f, int):
-                    print("{:d}".format(f), end="")
+            for f in range(x):
+                if isinstance(my_list[f], int):
+                    print("{:d}".format(my_list[f]), end="")
                     m += 1
-                else:
-                    continue
             print()
             return m
     except TypeError:
         return m
+
