a66bd6546c9c7bfeefbebf26b62ee275aec6987e
mahmoud <ramadanmahmoud945@gmail.com>
2024-04-26 14:23:57 +0300

    2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/in/python3
+'''class'''
+
+
+class Square:
+    '''square class'''
+    def __init__(self, size=0):
+        try:
+            if isinstance(size, int) and size >= 0:
+                self.__size = size
+        except TypeError:
+            print("size must be an integer")
+        except ValueError:
+            print("size must be >= 0")
