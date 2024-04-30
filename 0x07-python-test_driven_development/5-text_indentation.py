454ac3149c5a54ef301caafb6b2f1b082eb61fb5
mahmoud <ramadanmahmoud945@gmail.com>
2024-04-30 16:22:57 +0300

    345

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,18 +4,23 @@
 
 def text_indentation(text):
     '''print'''
-    a = 0
+    # a = 0
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    for i in text:
-        a += 1
-        if i in ['.', '?', ':']:
-            print("{:s}".format(i), end="\n\n")
-        else:
-            if (text[a - 2] in ['.', '?', ':', '\n']) and i == ' ':
-                continue
-            else:
-                print("{}".format(i), end="")
+    # for i in text:
+    #     a += 1
+    #     if i in ['.', '?', ':']:
+    #         print("{}".format(i), end="\n\n")
+    #     else:
+    #         if (text[a - 2] in ['.', '?', ':', '\n']) and i == ' ':
+    #             continue
+    #         else:
+    #             print("{}".format(i), end="")
+    for i in ['.', '?', ':']:
+        text = (i + "\n\n").join(
+            [line.strip(" ") for line in text.split(i)])
+
+    print("{}".format(text), end="")
 
 
 if __name__ == "__main__":
