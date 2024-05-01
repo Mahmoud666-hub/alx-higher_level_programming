e41f52db52341bcfacf58931efdd3b50e037717f
mahmoud <ramadanmahmoud945@gmail.com>
2024-05-01 20:25:22 +0300

    3-

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+'''rectangle'''
+
+
+class Rectangle:
+    '''new class'''
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return (self.__width + self.__height) * 2
+
+    def __str__(self):
+        h = "#" * self.__width
+        n = ""
+        for i in range(self.__height):
+            if i < (self.__height - 1):
+                m = h + "\n"
+            else:
+                m = h
+            n += m
+        return n
