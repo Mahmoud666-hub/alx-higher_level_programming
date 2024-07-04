ca04d03b8bf6f1b62861df059438fbf03494232e
mahmoud <ramadanmahmoud945@gmail.com>
2024-07-04 16:57:21 +0300

    ede

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- nnml
-SELECT tv_shows.title, tv_show_genres.genre_id DEFAULT NULL
+SELECT tv_shows.title, COALESCE(tv_show_genres.genre_id, NULL) AS genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
