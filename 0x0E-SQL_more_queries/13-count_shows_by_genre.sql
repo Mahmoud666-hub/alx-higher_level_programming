419008e20190c2bfdce75a4a4ed58b6ac347cb33
mahmoud <ramadanmahmoud945@gmail.com>
2024-07-04 21:44:32 +0300

    eLCCC

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,13 +1,13 @@
 -- jnnunu
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY genre
--- HAVING COUNT(tv_show_genres.genre_id) IS NOT NULL
-ORDER BY number_of_shows DESC;
 -- SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 -- FROM tv_genres
--- INNER JOIN tv_show_genres
--- ON tv_genres.id = tv_show_genres.genre_id
+-- LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 -- GROUP BY genre
+-- HAVING COUNT(tv_show_genres.genre_id) IS NOT NULL
 -- ORDER BY number_of_shows DESC;
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
