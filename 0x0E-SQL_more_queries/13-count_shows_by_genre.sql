96ee9c3593e320c830fe3f1b5a35cb663161fb16
mahmoud <ramadanmahmoud945@gmail.com>
2024-07-04 21:27:30 +0300

    ede

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- jnnunu
-SELECT name AS genre, IFNULL(COUNT(genre_id), NULL) AS number_of_shows
+SELECT name AS genre, COUNT(genre_id) AS number_of_shows
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY COUNT(genre_id)
-WHERE number_of_shows IS NOT NULL
+GROUP BY genre
+HAVING COUNT(tv_show_genres.genre_id) IS NOT NULL
 ORDER BY number_of_shows DESC;
