2d4970176f69f656e8c370b6719c4ead2a1f225e
mahmoud <ramadanmahmoud945@gmail.com>
2024-07-04 16:51:23 +0300

    ede

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- nnml
+SELECT tv_shows.title, tv_show_genres.genre_id DEFAULT NULL
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
