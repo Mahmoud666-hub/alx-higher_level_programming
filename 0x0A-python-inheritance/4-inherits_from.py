3edb78a671fb29718ecd465380204ebf65ff323d
mahmoud <ramadanmahmoud945@gmail.com>
2024-07-11 12:45:26 +0300

    jhu

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""ygygy"""
+
+
+def inherits_from(obj, a_class):
+    """lll"""
+    return issubclass(type(True), object) or type(obj) is a_class
+
+# print(issubclass(type(True), object))
