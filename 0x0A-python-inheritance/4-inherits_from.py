2a2771baafc972a898ac19b3c6b802bdaa415762
mahmoud <ramadanmahmoud945@gmail.com>
2024-07-11 13:08:49 +0300

    jp-l

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,6 +4,6 @@
 
 def inherits_from(obj, a_class):
     """lll"""
-    return issubclass(type(obj), a_class) or isinstance(obj, a_class)
+    return isinstance(obj, a_class)
 
 # print(issubclass(type(True), object))
